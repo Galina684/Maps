c68207d9393660c3fbb1ecebf030b1aa93639ed4
Galina684 <galina684@gmail.com>
2023-09-27 13:51:41 +0300

    Домашка ХэшМэп

---
+++ src/main/java/pro/sky/EmployeeBookmaps/EmployeeBookMapsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeBookmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookMapsApplication.class, args);
+	}
+
+}
